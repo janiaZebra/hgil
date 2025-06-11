db5880508f6bec9f943e2afbb33f0d27e21c0057
julianzebra <julian.ania@zebraventures.eu>
2025-06-11 16:03:04 +0200

    Quitar debug

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def send_message_to_whatsapp(phone, text):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
