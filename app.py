dcc091bf3bc2f7b3477e2dc24fcf1234c8547e1d
julianzebra <julian.ania@zebraventures.eu>
2025-06-11 14:57:18 +0200

    Actualizado Dockerfile

--- app.py
+++ app.py
@@ -97,4 +97,6 @@ def send_message_to_whatsapp(phone, text):
 
 
 if __name__ == "__main__":
-    app.run(debug=FLASK_DEBUG_MODE, port=FLASK_PORT)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
